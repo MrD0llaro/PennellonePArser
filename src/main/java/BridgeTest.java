24f12f7789b5e71e98a1941685ae798a741477a4
Gianluca Musa <gianlcua.musa@infogroup.it>
2020-06-18 19:24:00 +0200

    fix

--- src/main/java/BridgeTest.java
+++ src/main/java/BridgeTest.java
@@ -2,7 +2,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -33,7 +36,7 @@ public class BridgeTest {
 
 		Document doc = renderPage("https://dds.bridgewebs.com/bsol2/ddummy.htm?file=https://areaprivata.bboitalia.it/web/upload/tornei-online/5695-1590343636.5782.lin");
 		System.out.println(doc.title());
-		Elements newsHeadlines = doc.select("#miniMakeableContracts");
+		Elements newsHeadlines = doc.select("makeableContracts");
 		for (Element headline : newsHeadlines) {
 			System.out.println(
 					headline.attr("title"));
@@ -41,8 +44,8 @@ public class BridgeTest {
 		headline.absUrl("href"));
 
 		}
-		System.out.println(doc.head().toString());
-		System.out.println(doc.body().toString());
+
+		//System.out.println(doc.body().toString());
 
 	}
 
@@ -50,13 +53,22 @@ public class BridgeTest {
 
 
 	public static Document renderPage(String filePath) {
-		System.setProperty("phantomjs.binary.path", "libs/phantomjs"); // path to bin file. NOTE: platform dependent
+
+		System.setProperty("phantomjs.binary.path", "/Users/IG01910/Documents/workspace-test/phantomjs-2.1.1-macosx/bin/phantomjs"); // path to bin file. NOTE: platform dependent
 		PhantomJSDriver ghostDriver = new PhantomJSDriver();
+
 		try {
 			ghostDriver.get(filePath);
+			System.out.println("te table ");
+			Thread.sleep(5000);
+
+			System.out.println(ghostDriver.findElement(By.id("makeableContracts")).getText());
 			return Jsoup.parse(ghostDriver.getPageSource());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		} finally {
 			ghostDriver.quit();
 		}
+		return null;
 	}
 }
